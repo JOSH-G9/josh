c3011bfac5ea8a38e414edee06cb9360f78d7799
Cristianald00 <cristianald00@hotmail.com>
2018-03-24 15:23:13 -0400

    h

--- core/bin/ajax/goNewJob.php
+++ core/bin/ajax/goNewJob.php
@@ -3,15 +3,15 @@ if (!empty($_POST['description']) and !empty($_POST['category'])) {
   $db = new Connection();
 
   $description = $db->real_escape_string(strtolower($_POST['description']));
-  $pass =  $db->real_escape_string($_POST['category']);
+  $category =  $db->real_escape_string($_POST['category']);
   $user_id =  $_SESSION['app_id'];
   if (isset($_POST['video_url'])) {
-    $pass =  $db->real_escape_string($_POST['video_url']);
+    $video_url =  $db->real_escape_string($_POST['video_url']);
   }
 
   $sql = $db->query("SELECT id FROM user WHERE id='$user_id';");
   if ($db->rows($sql) > 0) {
-    $db->query("In ")
+    $db->query("INSERT INTO jobs(description,video_url,poster_id,category_id) VALUES('$description','$video_url','$user_id','$category');");
   } else {
     $HTML = '<div class="alert-danger"><h4><strong>Error: </strong></h4><p>The email doesnt exist. </p></div>';
   }
