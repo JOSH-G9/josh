b1106d2ed2d9bde0427876b028a8d057186a6c72
NimaJT <nima.jts@gmail.com>
2018-03-24 11:54:50 -0700

    html
    htmlnima

--- html/index.php
+++ html/index.php
@@ -2,15 +2,14 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <link rel="stylesheet" href="views/css/styles.css">
   </head>
   <body>
     <div id="body">
       <header>
-
+ <p id="titleName">Job Shadowing</p>
       </header>
       <div id="main">
-        
       </div>
       <footer></footer>
     </div>
