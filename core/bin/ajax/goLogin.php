c2c674234d302db4c82e8a97ec73c130b9fc1c9c
Cristianald00 <cristianald00@hotmail.com>
2018-03-24 15:54:25 -0400

    login mysql

--- core/bin/ajax/goLogin.php
+++ core/bin/ajax/goLogin.php
@@ -4,19 +4,13 @@ if (!empty($_POST['email']) and !empty($_POST['pass'])) {
   $email = $db->real_escape_string(strtolower($_POST['email']));
   $pass =  $db->real_escape_string($_POST['pass']);
 
-  $sql = $db->query("SELECT id FROM customers WHERE email='$email';");
+  $sql = $db->query("SELECT id FROM user WHERE email='$email';");
   if ($db->rows($sql) > 0) {
-    $sql = $db->query("SELECT id FROM customers WHERE email='$email' AND pass='$pass';");
+    $sql = $db->query("SELECT id FROM user WHERE email='$email' AND password='$pass';");
     if ($db->rows($sql) > 0) {
       $user_id = $db->browse($sql)[0];
-      $sql2 = $db->query("SELECT active FROM customers WHERE id='$user_id' LIMIT 1;");
-      if ($db->browse($sql2)[0] == 1) {
-        $_SESSION['app_id'] = $user_id;
-        $HTML = '<div class="alert-warning"><h4><strong>Error: </strong></h4><p>Successfully logigged in.</p></div>';
-      }else {
-        $HTML = '<div class="alert-warning"><h4><strong>Error: </strong></h4><p>You can not log in until your registration request has been approved.
-</p></div>';
-      }
+      $_SESSION['app_id'] = $user_id;
+      $HTML = '<div class="alert-warning"><h4><strong>Error: </strong></h4><p>Successfully logigged in.</p></div>';
       $db->drop($sql2);
     } else {
       $HTML = '<div class="alert-danger"><h4><strong>Error: </strong></h4><p>The email and password dont match. </p></div>';
