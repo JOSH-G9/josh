d118964118031c87d98c6fae82be387538f09b81
Logan Junhwi Kim <logan.kim0212@gmail.com>
2018-03-24 13:09:24 -0400

    logan

--- html/general/header.php
+++ html/general/header.php
@@ -1,3 +1,18 @@
 <header>
-  <a href="?view=home">nimas page</a><a href="?view=searchPage">logans page</a><a href="?view=results">Andy</a>
+    <div class="container">
+        <div id="branding">
+            <h1>
+                <a href="#">Job <span class="highlight">Shadowing</span></a>
+            </h1>
+        </div>
+        <form id="search-form" action = "" method="post">
+            <input type="text" name="search" placeholder="Search events, friends and more"/>
+            <a><i class="fa fa-search" aria-hidden="true"></i></a>
+        </form>
+        <nav>
+        </nav>
+    </div>
+    <a href="?view=home">nimas page</a>
+    <a href="?view=searchPage">logans page</a>
+    <a href="?view=results">Andy</a>
 </header>
