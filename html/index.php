eaf3ef3b68800f4af82a4b6556ad9a919f7e838b
NimaJT <nima.jts@gmail.com>
2018-03-24 12:19:39 -0700

    j

--- html/index.php
+++ html/index.php
@@ -15,22 +15,23 @@
             <form action = "http://www.example.com/search.php" method = "post">
                 <input class="search_input" maxlength="30" autocomplete="off" name="SearchHome" type = "text" placeholder = "Search job shadowing"/>
                 <input class="search_button" type = "submit" value = "Search"/>
-                <div id="bttns">
-                <div id="website1">
-                  <link id="link1" ahref="/css/master.css">
-                </div>
-                <div id="website2">
-                   <link id="website2" ahref="/css/master.css">
-                </div>
-                <div id="website3">
-                  <link id="website3" ahref="/css/master.css">
-                </div>
-                 <div id="website4">
-                   <link id="website4" ahref="/css/master.css">
-                 </div>
-                 
-                </div>
+                
             </form>
+            <div id="links">
+            <div id="website1">
+              <link id="link1" ahref="/css/master.css">
+            </div>
+            <div id="website2">
+               <link id="website2" ahref="/css/master.css">
+            </div>
+            <div id="website3">
+              <link id="website3" ahref="/css/master.css">
+            </div>
+             <div id="website4">
+               <link id="website4" ahref="/css/master.css">
+             </div>
+             
+            </div>
         </div>
 =======
 >>>>>>> b1106d2ed2d9bde0427876b028a8d057186a6c72
