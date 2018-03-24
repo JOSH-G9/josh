2e584b26ce4c6e24bc5c8126aa88b61a46740413
Logan Junhwi Kim <logan.kim0212@gmail.com>
2018-03-24 11:55:26 -0400

    logan

--- html/index.php
+++ html/index.php
@@ -10,7 +10,12 @@
 
       </header>
       <div id="main">
-        
+        <div id="search">
+            <form action = "http://www.example.com/search.php" method = "post">
+                <input class="search_input" maxlength="50" autocomplete="off" name="SearchHome" type = "text" placeholder = "Search job shadowing"/>
+                <input class="search_button" type = "submit" value = "Search"/>
+            </form>
+        </div>
       </div>
       <footer></footer>
     </div>
