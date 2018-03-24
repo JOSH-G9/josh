5012a89166c460e8e2f27a138b2e5f38f2dcff4a
Logan Junhwi Kim <logan.kim0212@gmail.com>
2018-03-24 13:45:56 -0400

    logan

--- html/pages/home.php
+++ html/pages/home.php
@@ -1,35 +1,25 @@
-
 <div id="main">
-  <div id="search">
-
- <div id="main">
-<div id="titleName">
-  Job Shadowing
-</div>
-<div id="search">
-
-    <form action = "?page=searchPage" method = "post">
-
-    <form action = "http://www.example.com/search.php" method = "post">
-
-        <input class="search_input" maxlength="30" autocomplete="off" name="SearchHome" type = "text" placeholder = "Search job shadowing"/>
-        <input class="search_button" type = "submit" value = "Search"/>
-    </form>
+    <div id="titleName">
+        Job Shadowing
+    </div>
+    <div id="search">
+        <form action = "?page=searchPage" method = "post">
+            <input class="search_input" maxlength="30" autocomplete="off" name="SearchHome" type = "text" placeholder = "Search job shadowing"/>
+            <input class="search_button" type = "submit" value = "Search"/>
+        </form>
     </div>
     <div id="menu">
-    <div id="website1">
-      <link class="link" ahref="/css/master.css">
+        <div id="link">
+            <link class="link" ahref="/css/master.css">
+        </div>
+        <div id="link">
+            <link class="link" ahref="/css/master.css">
+        </div>
+        <div id="link">
+            <link class="link" ahref="/css/master.css">
+        </div>
+        <div id="link">
+            <link class="link" ahref="/css/master.css">
+        </div>
     </div>
-    <div id="website2">
-       <link class="link" ahref="/css/master.css">
-    </div>
-    <div id="website3">
-      <link class="link" ahref="/css/master.css">
-    </div>
-     <div id="website4">
-       <link class="link" ahref="/css/master.css">
-     </div>
-
-    </div>
-
 </div>
