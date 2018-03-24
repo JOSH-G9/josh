5acc76ddc0e1565a753f6e49fa5220fe9cf56fd3
NimaJT <nima.jts@gmail.com>
2018-03-24 12:59:05 -0700

    nima

--- html/index.php
+++ html/index.php
@@ -19,7 +19,7 @@
       <div id="main">
         <div id="search">
             <form action = "http://www.example.com/search.php" method = "post">
-                <input class="search_input" maxlength="30" autocomplete="off" name="SearchHome" type = "text" placeholder = "Search job shadowing"/>
+                <input class="search_input" maxlength="30" autocomplete="off" name="SearchHome" type = "text" placeholder = "Search Job shadowing"/>
                 <input class="search_button" type = "submit" value = "Search"/>
             </form>
             <div id="menu">
