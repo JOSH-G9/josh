4b39abcce29c31e1b8a90bda8255c61b66d6004e
Cristianald00 <cristianald00@hotmail.com>
2018-03-24 14:02:46 -0400

    login and newJob pages

---
+++ html/pages/login.php
@@ -0,0 +1,19 @@
+<div id="loginPage">
+  <div id="header-createJob">
+
+  </div>
+  <div id="body-createJob">
+    <form class="" action="index.html" method="post">
+      <table>
+        <tr>
+          <th><label for="email">Email:</label></th>
+          <td><input type="text" name="email" value=""></td>
+        </tr>
+        <tr>
+          <th><label for="password">Password:</label></th>
+          <td><input type="password" name="password" value=""></td>
+        </tr>
+      </table>
+    </form>
+  </div>
+</div>
--- html/pages/newJob.php
+++ html/pages/newJob.php
@@ -1,21 +1,19 @@
-<div id="search">
-    <form action = "http://www.example.com/search.php" method = "post">
-        <input class="search_input" maxlength="30" autocomplete="off" name="SearchHome" type = "text" placeholder = "Search job shadowing"/>
-        <input class="search_button" type = "submit" value = "Search"/>
+<div id="createJob">
+  <div id="header-createJob">
+
+  </div>
+  <div id="body-createJob">
+    <form class="" action="index.html" method="post">
+      <table>
+        <tr>
+          <th><label for="first">First name:</label></th>
+          <td><input type="text" name="first" value=""></td>
+        </tr>
+        <tr>
+          <th><label for="first">First name:</label></th>
+          <td><input type="text" name="first" value=""></td>
+        </tr>
+      </table>
     </form>
-    <div id="menu">
-    <div id="website1">
-      <link class="link" ahref="/css/master.css">
-    </div>
-    <div id="website2">
-       <link class="link" ahref="/css/master.css">
-    </div>
-    <div id="website3">
-      <link class="link" ahref="/css/master.css">
-    </div>
-     <div id="website4">
-       <link class="link" ahref="/css/master.css">
-     </div>
-    </div>
-</div>
+  </div>
 </div>
