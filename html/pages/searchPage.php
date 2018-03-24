0c744c12241ecdff523bdea9df0c33c03c96d4f1
Logan Junhwi Kim <logan.kim0212@gmail.com>
2018-03-24 15:32:03 -0400

    d

--- html/pages/searchPage.php
+++ html/pages/searchPage.php
@@ -10,7 +10,7 @@
                 </div>
             </a>
         </div>
-        <div href="?page=results" id="content02" class="content">
+        <div id="content02" class="content">
             <a href="?page=results">
                 <div id="contents_title">
                     <h2>content 2</h2>
@@ -20,10 +20,30 @@
                 </div>
             </a>
         </div>
-        <div href="?page=results" id="content03" class="content">
+        <div id="content03" class="content">
             <a href="?page=results">
                 <div id="contents_title">
                     <h2>content 3</h2>
+                </div>
+                <div id="contents_description">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            </a>
+        </div>
+        <div id="content04" class="content">
+            <a href="?page=results">
+                <div id="contents_title">
+                    <h2>content 4</h2>
+                </div>
+                <div id="contents_description">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            </a>
+        </div>
+        <div id="content05" class="content">
+            <a href="?page=results">
+                <div id="contents_title">
+                    <h2>content 5</h2>
                 </div>
                 <div id="contents_description">
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
