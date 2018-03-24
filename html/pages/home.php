9626966a692dbd4c4b2faf294c14be368b0806df
NimaJT <nima.jts@gmail.com>
2018-03-24 13:33:25 -0700

    jh

--- html/pages/home.php
+++ html/pages/home.php
@@ -1,16 +1,10 @@
-<<<<<<< HEAD
- <div id="search">
-=======
-<header>
-  <a href="?page=home">nimas page</a><a href="?page=searchPage">logans page</a><a href="?page=results">Andy</a>
-</header>
-
-<div id="search">
->>>>>>> 612ba8e8ba7bd225af63ebbd6151ee94432dcdee
+<div id="main">
+  <div id="search">
     <form action = "http://www.example.com/search.php" method = "post">
         <input class="search_input" maxlength="30" autocomplete="off" name="SearchHome" type = "text" placeholder = "Search job shadowing"/>
         <input class="search_button" type = "submit" value = "Search"/>
     </form>
+    </div>
     <div id="menu">
     <div id="website1">
       <link class="link" ahref="/css/master.css">
@@ -26,6 +20,6 @@
      </div>
 
     </div>
-</div>
+
 >>>>>>> aa747c3f9128e6f44e2232502ced336960af6bff
 </div>
