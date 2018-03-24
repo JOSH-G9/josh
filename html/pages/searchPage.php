b4d57b37a090fa5b064ee8b2c6397925f49c5681
Logan Junhwi Kim <logan.kim0212@gmail.com>
2018-03-24 12:53:13 -0400

    logan

--- html/pages/searchPage.php
+++ html/pages/searchPage.php
@@ -3,22 +3,35 @@
 <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="views/css/styles.css">
-    <title></title>
+    <link rel="stylesheet" href="views/css/font-awesome.min.css">
+    <title>Job shadowing</title>
 </head>
 <body>
     <div id="body">
         <header>
             <div class="container">
                 <div id="branding">
-                    <h1><a href="#">Job <span class="highlight">Shadowing</span></a></h1>
+                    <h1>
+                        <a href="#">Job <span class="highlight">Shadowing</span></a>
+                    </h1>
                 </div>
+                <form id="search-form" action = "" method="post">
+                    <input type="text" name="search" placeholder="Search events, friends and more"/>
+                    <a><i class="fa fa-search" aria-hidden="true"></i></a>
+                </form>
                 <nav>
                 </nav>
             </div>
         </header>
         <div id="main">
-            <div id="title">
-                <h1>Job Shadowing</h1>
+            <div id="searchOptions">
+                <form action="">
+                    <input type="checkbox" name="Option01" value="1">Option 1
+                    <input type="checkbox" name="Option02" value="2">Option 2
+                    <input type="checkbox" name="Option02" value="3">Option 3
+                    <input type="checkbox" name="Option02" value="4">Option 4
+                    <input type="checkbox" name="Option02" value="5">Option 5
+                </form>
             </div>
         </div>
         <footer></footer>
