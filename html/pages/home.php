742c77bda2f93586b9778307c29a75ccfc8baa37
Cristianald00 <cristianald00@hotmail.com>
2018-03-24 13:03:25 -0400

    home

--- html/pages/home.php
+++ html/pages/home.php
@@ -1,1 +1,23 @@
-Nima
+<div id="search">
+    <form action = "http://www.example.com/search.php" method = "post">
+        <input class="search_input" maxlength="30" autocomplete="off" name="SearchHome" type = "text" placeholder = "Search job shadowing"/>
+        <input class="search_button" type = "submit" value = "Search"/>
+    </form>
+    <div id="menu">
+    <div id="website1">
+      <link class="link" ahref="/css/master.css">
+    </div>
+    <div id="website2">
+       <link class="link" ahref="/css/master.css">
+    </div>
+    <div id="website3">
+      <link class="link" ahref="/css/master.css">
+    </div>
+     <div id="website4">
+       <link class="link" ahref="/css/master.css">
+     </div>
+
+    </div>
+</div>
+>>>>>>> aa747c3f9128e6f44e2232502ced336960af6bff
+</div>
