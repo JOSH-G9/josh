27d05da1b99bb066b9d1ce8db0c73d104cba31db
Logan Junhwi Kim <logan.kim0212@gmail.com>
2018-03-24 15:29:57 -0400

    c

--- html/pages/searchPage.php
+++ html/pages/searchPage.php
@@ -1,42 +1,34 @@
 <div id="main_search">
-    <div id="searchOptions">
-        <article>
-            <button class="accordion">Search Option</button>
-            <div class="panel">
-                <form method="post">
-                    <input type="checkbox" name="Option01" value="1">Option 1
-                    <input type="checkbox" name="Option02" value="2">Option 2
-                    <input type="checkbox" name="Option02" value="3">Option 3
-                    <input type="checkbox" name="Option02" value="4">Option 4
-                    <input type="checkbox" name="Option02" value="5">Option 5
-                </form>
-            </div>
-        </article>
-    </div>
     <div id="contents">
         <div id="content01" class="content">
-            <div id="contents_title">
-                <h2>content 1</h2>
-            </div>
-            <div id="contents_description">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </div>
+            <a href="?page=results">
+                <div id="contents_title">
+                    <h2>content 1</h2>
+                </div>
+                <div id="contents_description">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            </a>
         </div>
-        <div id="content02" class="content">
-            <div id="contents_title">
-                <h2>content 2</h2>
-            </div>
-            <div id="contents_description">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </div>
+        <div href="?page=results" id="content02" class="content">
+            <a href="?page=results">
+                <div id="contents_title">
+                    <h2>content 2</h2>
+                </div>
+                <div id="contents_description">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            </a>
         </div>
-        <div id="content03" class="content">
-            <div id="contents_title">
-                <h2>content 3</h2>
-            </div>
-            <div id="contents_description">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </div>
+        <div href="?page=results" id="content03" class="content">
+            <a href="?page=results">
+                <div id="contents_title">
+                    <h2>content 3</h2>
+                </div>
+                <div id="contents_description">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            </a>
         </div>
     </div>
 </div>
