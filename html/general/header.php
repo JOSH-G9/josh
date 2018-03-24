cde7558375a4099113e70c4db6335c19cb7457b2
Cristianald00 <cristianald00@hotmail.com>
2018-03-24 14:15:57 -0400

    rod

--- html/general/header.php
+++ html/general/header.php
@@ -10,18 +10,11 @@
             <a><i class="fa fa-search" aria-hidden="true"></i></a>
         </form>
         <nav>
-<<<<<<< HEAD
-            <a href="?page=home">|  nimas page</a>
-            <a href="?page=searchPage">|  logans page</a>
-            <a href="?page=results">|  Andy</a>
-            <a href="?page=newJob">|  Rod</a>
-            <a href="?page=newJob">|  New job</a>
-=======
->>>>>>> 7f83fffb274c843596a9d1c6b9a2515726576c78
             <a href="?page=home">|  nima</a>
             <a href="?page=searchPage">|  logan</a>
             <a href="?page=newJob">|  Andy</a>
             <a href="?page=results">|  Rod</a>
+            <a href="?page=newJob">|  New job</a>
             <a href="?page=login">|  Login</a>
         </nav>
     </div>
