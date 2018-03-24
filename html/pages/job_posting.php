2531d1cb17d20e45ae819ba4fce964a2b3e2fdb0
KhoaHoAndy <nhatkhoa2811@gmail.com>
2018-03-24 13:49:33 -0400

    Andy

---
+++ html/pages/job_posting.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+<title>Job_shadowing</title>
+<link href="style.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+</style>
+</head>
+<body>
+<form method="post">
+<header>
+<h1 id="title">Job Posting</h1>
+</header>
+<div id="Container">
+<h2 id="Subject">Subject</h2>
+<h3 id="Name">Name</h3>
+<video width="320" height="240" controls>
+   <a href="https://youtu.be/AX6WkaOxk_E" ></a>
+Your browser does not support the video tag.
+</video>
+<p id="decription">This is decription</p>
+</div>
+	
+		<input name="Submit1" type="submit" value="Send Email">
+		<input name="Submit1" type="submit" value="Apply" />
+		</form>
+<footer>
+<h5>&copy;&nbsp;&nbsp; Copyright - Job Shadowing</h5>
+</footer>
+
+</body>
+</html>
