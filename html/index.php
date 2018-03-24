c739d42db716eee576a5f465cc1e7ebda25b99ea
Cristianald00 <cristianald00@hotmail.com>
2018-03-24 13:00:52 -0400

    index

--- html/index.php
+++ html/index.php
@@ -7,19 +7,18 @@
   <body>
     <div id="body">
       <?php
-<<<<<<< HEAD
      include('html/general/header.php');
-=======
-      include('html/general/header.php');
->>>>>>> 625b5929107b6445e9f6a6571182da420f076630
       ?>
+<<<<<<< HEAD
       <header>
-        <div id="titleName">Job Shadowing</div>
+ <div id="titleName">Job Shadowing</div>
       </header>
+=======
+>>>>>>> aa747c3f9128e6f44e2232502ced336960af6bff
       <div id="main">
         <div id="search">
             <form action = "http://www.example.com/search.php" method = "post">
-                <input class="search_input" maxlength="30" autocomplete="off" name="SearchHome" type = "text" placeholder = "Search Job shadowing"/>
+                <input class="search_input" maxlength="30" autocomplete="off" name="SearchHome" type = "text" placeholder = "Search job shadowing"/>
                 <input class="search_button" type = "submit" value = "Search"/>
             </form>
             <div id="menu">
@@ -38,6 +37,25 @@
 
             </div>
         </div>
+<<<<<<< HEAD
+<<<<<<< HEAD
+
+=======
+            <div id="menu">
+                <div id="website1">
+                    <link id="link1" ahref="/css/master.css">
+                </div>
+                <div id="website2">
+                    <link id="website2" ahref="/css/master.css">
+                </div>
+                <div id="website3">
+                    <link id="website3" ahref="/css/master.css">
+                </div>
+                <div id="website4">
+                    <link id="website4" ahref="/css/master.css">
+                </div>
+            </div>
+>>>>>>> aa747c3f9128e6f44e2232502ced336960af6bff
       </div>
       <footer>
         <?php
