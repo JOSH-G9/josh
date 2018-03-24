e58be5aa5c3979e86e29a93236d87a647c433a33
Cristianald00 <cristianald00@hotmail.com>
2018-03-24 12:52:25 -0400

    Add Controllers

--- html/index.php
+++ html/index.php
@@ -7,20 +7,13 @@
   <body>
     <div id="body">
       <?php
-      include('');
+      include('html/general/header.php');
       ?>
-      <header>
- <p id="titleName">Job Shadowing</p>
-      </header>
       <div id="main">
         <div id="search">
             <form action = "http://www.example.com/search.php" method = "post">
                 <input class="search_input" maxlength="30" autocomplete="off" name="SearchHome" type = "text" placeholder = "Search job shadowing"/>
                 <input class="search_button" type = "submit" value = "Search"/>
-<<<<<<< HEAD
-
-=======
->>>>>>> 25d8d7fe37c92f6989dcc8dbe0e81bc5fd088361
             </form>
             <div id="links">
             <div id="website1">
@@ -37,9 +30,7 @@
              </div>
 
             </div>
->>>>>>> 30182f61a318901e73be7f4d51d02b2faa88f087
         </div>
-<<<<<<< HEAD
             <div id="menu">
                 <div id="website1">
                     <link id="link1" ahref="/css/master.css">
