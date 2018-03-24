e829274169e9caec4d45721645e67abcc00f09c6
Logan Junhwi Kim <logan.kim0212@gmail.com>
2018-03-24 13:34:28 -0400

    logan

--- html/pages/searchPage.php
+++ html/pages/searchPage.php
@@ -1,38 +1,37 @@
-        <div id="main">
-            <div id="searchOptions">
-                <form action="">
-                    <input type="checkbox" name="Option01" value="1">Option 1
-                    <input type="checkbox" name="Option02" value="2">Option 2
-                    <input type="checkbox" name="Option02" value="3">Option 3
-                    <input type="checkbox" name="Option02" value="4">Option 4
-                    <input type="checkbox" name="Option02" value="5">Option 5
-                </form>
+<div id="main">
+    <div id="searchOptions">
+        <form action="">
+            <input type="checkbox" name="Option01" value="1">Option 1
+            <input type="checkbox" name="Option02" value="2">Option 2
+            <input type="checkbox" name="Option02" value="3">Option 3
+            <input type="checkbox" name="Option02" value="4">Option 4
+            <input type="checkbox" name="Option02" value="5">Option 5
+        </form>
+    </div>
+    <div id="contents">
+        <div id="content01">
+            <div id="contents_title">
+                <h2>content 1</h2>
             </div>
-            <div id="contents">
-                <div id="content01">
-                    <div id="contents_title">
-                        <h2>content 1</h2>
-                    </div>
-                    <div id="contents_description">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    </div>
-                </div>
-                <div id="content02">
-                    <div id="contents_title">
-                        <h2>content 2</h2>
-                    </div>
-                    <div id="contents_description">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    </div>
-                </div>
-                <div id="content03">
-                    <div id="contents_title">
-                        <h2>content 3</h2>
-                    </div>
-                    <div id="contents_description">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    </div>
-                </div>
+            <div id="contents_description">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
         </div>
-        <footer></footer>
+        <div id="content02">
+            <div id="contents_title">
+                <h2>content 2</h2>
+            </div>
+            <div id="contents_description">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+        </div>
+        <div id="content03">
+            <div id="contents_title">
+                <h2>content 3</h2>
+            </div>
+            <div id="contents_description">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+        </div>
+    </div>
+</div>
