e96e2f8ef8c6354b520d0686a800dd11ea49d605
Cristianald00 <cristianald00@hotmail.com>
2018-03-24 11:37:58 -0400

    Html template

---
+++ html/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <div id="body">
+      <header>
+
+      </header>
+      <div id="main">
+        
+      </div>
+      <footer></footer>
+    </div>
+  </body>
+</html>
