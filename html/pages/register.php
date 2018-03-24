e6befb764a0345faf624eed37e2c6cf0f96ed006
Logan Junhwi Kim <logan.kim0212@gmail.com>
2018-03-24 16:10:51 -0400

    s

--- html/pages/register.php
+++ html/pages/register.php
@@ -1,18 +1,18 @@
 <div id="main_registration">
     <article>
-        <h2><strong>Member Registration</strong></h2>
+        <h2 id="register_title"><strong>Member Registration</strong></h2>
         <form method="post" id="Registration" name="Registration" onsubmit="return validateForm()">
-            <label for="FirstName">First Name:</label><br />
+            <label for="FirstName">First Name</label><br />
             <input id="FirstName" type="text" name="FirstName" placeholder="Donald"><br />
-            <label for="LastName">Last Name:</label><br />
+            <label for="LastName">Last Name</label><br />
             <input id="LastName" type="text" name="LastName" placeholder="Trump"><br />
-            <label for="Address">Address:</label><br />
+            <label for="Address">Address</label><br />
             <input id="Address" type="text" name="Address" placeholder="123 Yonge St."><br />
-            <label for="City">City:</label><br />
+            <label for="City">City</label><br />
             <input id="City" type="text" name="City" placeholder="Toronto"><br />
-            <label for="PostalCode">Postal Code:</label><br />
+            <label for="PostalCode">Postal Code</label><br />
             <input id="PostalCode" type="text" name="PostalCode" placeholder="X0X0X0"><br />
-            <label for="Province">Province:</label><br />
+            <label for="Province">Province</label><br />
             <select id="Province" name="Province">
                 <option value="Alberta">Alberta</option>
                 <option value="British Columbia">British Columbia</option>
@@ -27,13 +27,13 @@
                 <option value="Saskatchewan">Saskatchewan</option>
                 <option value="Yukon">Yukon</option>
             </select><br />
-            <label for="Age">Age:</label><br />
+            <label for="Age">Age</label><br />
             <input id="Age" type="number" name="Age" placeholder="18"><br />
-            <label for="Password">Password:</label><br />
+            <label for="Password">Password</label><br />
             <input id="Password" type="password" name="Password"><br />
-            <label for="Confirm">Confirm:</label><br />
+            <label for="Confirm">Confirm</label><br />
             <input id="Confirm" type="password" name="confirm"><br />
-            <label for="Email">Email:</label><br />
+            <label for="Email">Email</label><br />
             <input id="Email" type="text" name="Email" placeholder="email@example.com"><br />
             <input id="submit" type="submit" name="submit" value="Register Now">
             <input id="reset" type="reset" name="clear" value="Clear Form">
